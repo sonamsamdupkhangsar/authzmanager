7b773b1ac3a2190ce305d2795a7f1d7378e4609a
Sonam Samdupkhangsar <sonamsamdupkhangsar@Sonams-MacBook-Pro.local>
2024-12-25 13:27:13 -0700

    add new client auth methods and grant types from 1.4.1 SAS

--- app/src/main/java/me/sonam/authzmanager/oauth2/AuthorizationGrantType.java
+++ app/src/main/java/me/sonam/authzmanager/oauth2/AuthorizationGrantType.java
@@ -2,18 +2,19 @@ package me.sonam.authzmanager.oauth2;
 
 import org.springframework.util.Assert;
 
+import java.io.Serial;
 import java.io.Serializable;
 
 public final class AuthorizationGrantType implements Serializable {
+    @Serial
     private static final long serialVersionUID = 610L;
     public static final AuthorizationGrantType AUTHORIZATION_CODE = new AuthorizationGrantType("authorization_code");
-    public static final AuthorizationGrantType REFRESH_TOKEN = new AuthorizationGrantType("refresh_token");
     public static final AuthorizationGrantType CLIENT_CREDENTIALS = new AuthorizationGrantType("client_credentials");
-    /** @deprecated */
-    @Deprecated
-    public static final AuthorizationGrantType PASSWORD = new AuthorizationGrantType("password");
-    public static final AuthorizationGrantType JWT_BEARER = new AuthorizationGrantType("urn:ietf:params:oauth:grant-type:jwt-bearer");
+    public static final AuthorizationGrantType REFRESH_TOKEN = new AuthorizationGrantType("refresh_token");
     public static final AuthorizationGrantType DEVICE_CODE = new AuthorizationGrantType("urn:ietf:params:oauth:grant-type:device_code");
+    public static final AuthorizationGrantType TOKEN_EXCHANGE = new AuthorizationGrantType("urn:ietf:params:oauth:grant-type:token-exchange");
+    public static final AuthorizationGrantType JWT_BEARER = new AuthorizationGrantType("urn:ietf:params:oauth:grant-type:jwt-bearer");
+
     private final String value;
 
     public AuthorizationGrantType(String value) {
--- app/src/main/java/me/sonam/authzmanager/oauth2/ClientAuthenticationMethod.java
+++ app/src/main/java/me/sonam/authzmanager/oauth2/ClientAuthenticationMethod.java
@@ -3,14 +3,18 @@ package me.sonam.authzmanager.oauth2;
 
 import org.springframework.util.Assert;
 
+import java.io.Serial;
 import java.io.Serializable;
 
 public final class ClientAuthenticationMethod implements Serializable {
+    @Serial
     private static final long serialVersionUID = 610L;
     public static final ClientAuthenticationMethod CLIENT_SECRET_BASIC = new ClientAuthenticationMethod("client_secret_basic");
     public static final ClientAuthenticationMethod CLIENT_SECRET_POST = new ClientAuthenticationMethod("client_secret_post");
     public static final ClientAuthenticationMethod CLIENT_SECRET_JWT = new ClientAuthenticationMethod("client_secret_jwt");
     public static final ClientAuthenticationMethod PRIVATE_KEY_JWT = new ClientAuthenticationMethod("private_key_jwt");
+    public static final ClientAuthenticationMethod TLS_CLIENT_AUTH = new ClientAuthenticationMethod("tls_client_auth");
+    public static final ClientAuthenticationMethod SELF_SIGNED_TLS_CLIENT_AUTH = new ClientAuthenticationMethod("self_signed_tls_client_auth");
     public static final ClientAuthenticationMethod NONE = new ClientAuthenticationMethod("none");
     private final String value;
 
